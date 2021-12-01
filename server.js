b9dc9071332a8a1dd4656b457122bcb7861abd7e
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-12-01 10:15:26 -0300

    arrumando app get 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const  path  = require('path');
 const app =  express()
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("./build"));
   app.get("/*", function(req, res) {
       res.sendFile(path.join(__dirname, "./build/index.html"));
     });
